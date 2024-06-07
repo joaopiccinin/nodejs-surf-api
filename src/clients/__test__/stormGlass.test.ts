6e6a299b198ebef01e8e5acd2d5ee77e1cbc761f
joaopiccinin <j.marcos8410@gmail.com>
2024-06-06 22:32:57 -0300

    normalizando os dados

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -8,7 +8,7 @@ describe('stormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixtures)
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixtures })
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
         expect(response).toEqual(stormGlassNormalized3HoursFixtures);
